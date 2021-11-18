db5fd8d01edcfd453b4fa8361a0b0ae86fd235ca
Tyler Petersen <71360850+Balderdash1010@users.noreply.github.com>
2021-11-17 21:02:10 -0700

    Utils folder and film titles sorted

--- characters/sw-chars.js
+++ characters/sw-chars.js
@@ -1,4 +1,5 @@
 import { people } from "../data/people.js";
+import { getLastNumber } from '../utils/index.js'
 
 const main = document.querySelector("#main");
 
@@ -61,11 +62,3 @@ function populateDOM(characters) {
 })
 }
 
-function getLastNumber(url) {
-  let end = url.lastIndexOf('/')
-  let start = end - 2
-  if (url.charAt(start) === '/'){
-    start++
-  }
-    return url.slice(start, end)
-}

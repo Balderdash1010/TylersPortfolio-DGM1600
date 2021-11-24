844e7252e05b2515ef9617d3de3d8dd3d9d06ca5
Tyler Petersen <71360850+Balderdash1010@users.noreply.github.com>
2021-11-23 19:51:13 -0700

    fixed star ship display added modal

--- starships/sw-starships.js
+++ starships/sw-starships.js
@@ -1,30 +1,47 @@
-import { starships } from '../data/starships.js'
-import { getLastNumber, removeChildren } from "../utils/index.js"
+import { starships } from "../data/starships.js";
+import { getLastNumber, removeChildren } from "../utils/index.js";
 
-const nav = document.querySelector('.nav')
-const navList = document.querySelector('.navlist')
-const shipView = document.querySelector('.shipViewer')
+const nav = document.querySelector(".nav");
+const navList = document.querySelector(".navlist");
+const shipView = document.querySelector(".shipViewer");
+
+const modal = document.querySelector(".modal");
+const closeButton = document.querySelector(".modal-close");
+const modalBackground = document.querySelector(".modal-background");
+const shipMessage = document.querySelector('.shipMessage')
+
+closeButton.addEventListener("click", () =>
+  modal.classList.toggle("is-active")
+);
+modalBackground.addEventListener("click", () =>
+  modal.classList.toggle("is-active")
+);
 
 function populateNav() {
-    starships.forEach(starship => {
-        let anchorWrap = document.createElement('a')
-        anchorWrap.href = '#'
-        let listItem = document.createElement('li')
-        listItem.textContent = starship.name
+  starships.forEach((starship) => {
+    let anchorWrap = document.createElement("a");
+    anchorWrap.href = "#";
+    let listItem = document.createElement("li");
+    listItem.textContent = starship.name;
 
-        anchorWrap.addEventListener('click', () => populateShipView(starship))
+    anchorWrap.addEventListener("click", () => populateShipView(starship));
 
-        anchorWrap.appendChild(listItem)
-        navList.appendChild(anchorWrap)
-    })
+    anchorWrap.appendChild(listItem);
+    navList.appendChild(anchorWrap);
+  });
 }
 
-populateNav()
+populateNav();
 
 function populateShipView(shipData) {
-    removeChildren(shipView)
-    let ShipImage = document.createElement('img')
-    let shipNum = getLastNumber(shipData.url)
-    ShipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
-    shipView.appendChild(ShipImage)
+  removeChildren(shipView);
+  let ShipImage = document.createElement("img");
+  let shipNum = getLastNumber(shipData.url);
+  ShipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`;
+  ShipImage.addEventListener("error", () => {
+    ShipImage.hidden = true;
+    modal.classList.toggle("is-active");
+    shipMessage.textContent = `${shipData.name} is currently unavailable`
+  });
+  shipView.appendChild(ShipImage);
 }
